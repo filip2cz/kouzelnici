ab7e9ebab1363b0926feebceed5743a9ff40befc
filip2cz <filip@fkomarek.eu>
2022-12-17 00:02:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kouzelnici
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] kouzelnici = { "funflame", "imaginez", "miraculo", "rimbleby", "septimus", "deepmire", "hypnotum", "tremenda" };
+
+            Kouzelnik[] kouzelnici = new Kouzelnik[]
+            {
+                new Kouzelnik("funflame", "red", "pruzina"),
+                new Kouzelnik("imaginez", "yellow", "mesic"),
+                new Kouzelnik("miraculo", "red", "mesic"),
+                new Kouzelnik("rimbleby", "blue", "modry mesic"),
+                new Kouzelnik("septimus", "yellow", "mesic"),
+                new Kouzelnik("deepmire", "blue", "hvezdy"),
+                new Kouzelnik("hypnotum", "red", "hvezdy"),
+                new Kouzelnik("tremenda", "red", "pruzina"),
+            };
+
+            bool computing = true;
+
+            while (computing)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < kouzelnici.Length; i++)
+                {
+                    int j = rnd.Next(i, kouzelnici.Length);
+                    Kouzelnik temp = kouzelnici[i];
+                    kouzelnici[i] = kouzelnici[j];
+                    kouzelnici[j] = temp;
+                }
+
+                foreach (Kouzelnik kouzelnik in kouzelnici)
+                {
+                    Console.WriteLine(kouzelnik);
+                }
+
+                if (true)
+                {
+                    if (true)
+                    {
+
+                    }
+                }
+                computing = false;
+            }
+
+            Console.ReadLine();
+        }
+        class Kouzelnik
+        {
+            public string Jmeno { get; set; }
+            public string Barva { get; set; }
+            public string Tvar { get; set; }
+            public override string ToString()
+            {
+                return $"{Jmeno}: {Barva}, {Tvar}";
+            }
+            public Kouzelnik(string jmeno, string barva, string tvar)
+            {
+                Jmeno = jmeno;
+                Barva = barva;
+                Tvar = tvar;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("kouzelnici")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alef-Group")]
+[assembly: AssemblyProduct("kouzelnici")]
+[assembly: AssemblyCopyright("Copyright © Alef-Group 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2cbe4c3d-9484-42c4-8986-325fc525f50c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
